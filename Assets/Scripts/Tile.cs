e38f5f0357590968619075c21ff68631345e1849
Jprummel <jordiprummel@live.nl>
2018-04-10 11:22:49 +0200

    - Initial tile script

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    /// <summary>
+    /// Open = buildable, Not_Usable = unusable tile, Occupied = Tile that contains tower
+    /// </summary>
+    public enum Status
+    {
+        OPEN,
+        NOT_USABLE,
+        OCCUPIED
+    }
+
+    public Status CurrentStatus;
+
+    private Tower m_Tower; //The tower on this tile
+
+	void Start () {
+        CurrentStatus = Status.OPEN;
+	}
+
+    void OnClick()
+    {
+        switch (CurrentStatus)
+        {
+            case Status.OPEN:
+                //Build tower if player has selected a tower and has enough money
+                break;
+            case Status.OCCUPIED:
+                //Open tower menu
+                break;
+        }
+    }
+}
