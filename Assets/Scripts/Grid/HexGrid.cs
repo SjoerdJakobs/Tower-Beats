09ce760e8d8f33b5917d4dc6639f8d46497d9c71
Jeffreeyy <17844@ma-web.nl>
2018-04-11 12:58:21 +0200

    Created a basic hexgrid

---
+++ Assets/Scripts/Grid/HexGrid.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexGrid : MonoBehaviour
+{
+    [Header("Prefab")]
+    [SerializeField] private Tile m_TilePrefab;
+    [Space]
+    [Header("Grid Settings")]
+    [SerializeField] private int m_GridWidth;
+    [SerializeField] private int m_GridHeight;
+    [Space]
+    [SerializeField] private float m_TileOffsetX;
+    [SerializeField] private float m_TileOffsetY;
+    [SerializeField] private float m_OffRowOffset;
+
+    [SerializeField] private Tile[,] m_Grid;
+
+
+    private void Awake()
+    {
+        CreateGrid();
+    }
+
+    private void CreateGrid()
+    {
+        float lastX = 0;
+        float lastY = 0;
+
+        // Fill the Grid.
+        m_Grid = new Tile[m_GridWidth, m_GridHeight];
+
+        for (int x = 0; x < m_GridWidth; x++)
+        {
+            GameObject row = new GameObject();
+            row.transform.parent = transform;
+            row.name = "HexRow | Row: " + x;
+
+            for (int y = 0; y < m_GridHeight; y++)
+            {
+                // Create Tile Component.
+                Tile tile = Instantiate(m_TilePrefab, row.transform, false) as Tile;
+
+                // Set random color for testing purposes.
+                tile.GetComponent<SpriteRenderer>().color = new Color(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f));
+
+                // Set Position on map.
+                tile.transform.localPosition = new Vector2((y%2 == 0 ? lastX : lastX + m_OffRowOffset), lastY);
+
+                // Set the name of the object.
+                tile.name = "HexTile | GridPos(x: " + x + " y: " + y + ")";
+
+                // Set the Tile's values;
+                tile.PositionInGrid = new Vector2Int(x, y);
+
+                // Set the Tile's state.
+                tile.CurrentState = TileState.OPEN;
+
+                // Add Tile to the Grid;
+                m_Grid[x, y] = tile;
+
+                // Update Y position.
+                lastY += m_TileOffsetY;
+            }
+
+            // Update X position.
+            lastX += m_TileOffsetX;
+
+            // Reset Y because X got updated, meaning that a new row will be made.
+            lastY = 0;
+        }
+
+        // Hide Prefab.
+        m_TilePrefab.gameObject.SetActive(false);
+    }
+
+    public Tile GetTile(int x, int y)
+    {
+        return m_Grid[x, y];
+    }
+}
---
+++ Assets/Scripts/Grid/Tile.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Open = buildable, Not_Usable = unusable tile, Occupied = Tile that contains tower
+/// </summary>
+public enum TileState
+{
+    OPEN,
+    NOT_USABLE,
+    OCCUPIED
+}
+
+public class Tile : MonoBehaviour
+{
+
+    private TileState m_CurrentState;
+    public TileState CurrentState { get; set; }
+
+    [SerializeField]private Vector2Int m_PositionInGrid;
+    public Vector2Int PositionInGrid { get { return m_PositionInGrid; } set { m_PositionInGrid = value; } }
+    public int X { get { return m_PositionInGrid.x; } set { m_PositionInGrid.x = value; } }
+    public int Y { get { return m_PositionInGrid.y; } set { m_PositionInGrid.y = value; } }
+
+    private Tower m_Tower; //The tower on this tile
+
+	void Start () {
+        m_CurrentState = TileState.OPEN;
+	}
+
+    void OnMouseDown()
+    {
+        switch (m_CurrentState)
+        {
+            case TileState.OPEN:
+                //Build tower if player has selected a tower and has enough money
+                break;
+            case TileState.OCCUPIED:
+                //Open tower menu
+                break;
+        }
+
+        print("Tile(Row: " + X + ", Position: " + Y + ") got pressed.");
+    }
+}
