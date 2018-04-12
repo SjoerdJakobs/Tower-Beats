ab0229424bc6376f3f20443b3003f80ba42a6b6c
Jeffreeyy <17844@ma-web.nl>
2018-04-12 12:20:14 +0200

    Grid Path Selection part 2.

---
+++ Assets/GridPathCreator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridPathCreator : MonoBehaviour
+{
+    [SerializeField] private List<Tile> m_SelectedTiles = new List<Tile>();
+
+    private bool m_IsSelectingTiles;
+
+    public void StartSelection()
+    {
+        Tile.s_OnTileClicked += TileClicked;
+        m_IsSelectingTiles = true;
+    }
+
+    public void StopSelection()
+    {
+        Tile.s_OnTileClicked -= TileClicked;
+        m_IsSelectingTiles = false;
+    }
+
+    private void TileClicked(Tile tile)
+    {
+        if(m_IsSelectingTiles)
+        {
+            if (!AlreadySelected(tile))
+            { 
+                m_SelectedTiles.Add(tile);
+            }
+        }
+    }
+
+    private bool AlreadySelected(Tile tile)
+    {
+        return m_SelectedTiles.Contains(tile);
+    }
+}
