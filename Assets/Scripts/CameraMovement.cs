02c63f8e603baeeb9f078a1f09a04408575fd3d9
Jprummel <jordiprummel@live.nl>
2018-04-26 12:34:46 +0200

    V0.1 Camera movement
    - Created camera movement (Hard set boundaries for now)
    - Duplicated the most complete scene for testing purposes

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,22 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class CameraMovement : MonoBehaviour {
 
-    [SerializeField] private float m_MoveSpeed;
+    [SerializeField] private float m_MinX;
+    [SerializeField] private float m_MaxX;
+    [SerializeField] private float m_MinY;
+    [SerializeField] private float m_MaxY;
+    [SerializeField] private float m_LerpSpeed; // Lower value is faster
 	
 	void Start () {
-		
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        MoveCamera();
 	}
 
     void MoveCamera()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector3 movePos = new Vector3(transform.position.x + x, transform.position.y + y,-10);
+        if (movePos.x < m_MaxX && movePos.x > m_MinX )
+        {
+            
+        }
+
+        if (movePos.x > m_MaxX)
+        {
+            movePos.x = m_MaxX;
+        }
+        else if(movePos.x < m_MinX)
+        {
+            movePos.x = m_MinX;
+        }
+
+        if(movePos.y > m_MaxY)
+        {
+            movePos.y = m_MaxY;
+        }
+        else if(movePos.y < m_MinY)
+        {
+            movePos.y = m_MinY;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, movePos, m_LerpSpeed);
     }
 }
