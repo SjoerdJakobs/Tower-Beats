00e3a5bda48ccd095b4d733e7a812a01832c7a95
Jeffreeyy <17844@ma-web.nl>
2018-05-16 17:18:10 +0200

    V0.2 Added EffectsManager

---
+++ Assets/Scripts/Managers/EffectsManager.cs
@@ -0,0 +1,40 @@
+﻿using Spine.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EffectType
+{
+    ENEMY_SPAWN
+}
+
+public class EffectsManager : MonoBehaviour
+{
+    public static EffectsManager s_Instance;
+    private ObjectPool m_ObjectPool;
+    [SerializeField] private SkeletonAnimation m_Prefab;
+
+    private void Awake()
+    {
+        if (s_Instance == null)
+            s_Instance = this;
+        else
+            Destroy(gameObject);
+
+        m_ObjectPool = ObjectPoolManager.s_Instance.GetObjectPool(m_Prefab.gameObject, 10, 5, 5, 10);
+    }
+
+    public void SpawnEffect(EffectType type, Vector2 position)
+    {
+        SkeletonAnimation anim = m_ObjectPool.GetFromPool().GetComponent<SkeletonAnimation>();
+
+        switch(type)
+        {
+            case EffectType.ENEMY_SPAWN:
+                anim.AnimationName = "Spawn FX";
+            break;
+        }
+
+        anim.transform.position = position;
+    }
+}
