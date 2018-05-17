bb01baa75abf567b4f9af8248171078f63a85061
Jprummel <jordiprummel@live.nl>
2018-05-17 18:56:37 +0200

    V0.2 Changed synth tower to lead tower
    - Changed Synth tower to lead tower (Code also reflects this)

---
+++ Assets/Scripts/Towers/Tower Types/LeadTower.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeadTower : Tower
+{
+    private TowerProjectile m_towerProjectileData;
+    private ObjectPool m_pool;
+    public override void Awake()
+    {
+        base.Awake();
+        m_pool = ObjectPoolManager.s_Instance.GetObjectPool(attackProjectile, 20, 5, 5, 20, true);
+        GetRMS.s_LeadCue += Attack;
+    }
+
+    public override void Attack()
+    {
+        base.Attack();
+
+        if (m_ReadyToAttack && m_Target != null)
+        {
+            m_towerProjectileData = m_pool.GetFromPool().GetComponent<TowerProjectile>();
+            m_towerProjectileData.SetNewVars(transform.position, m_Target, TowerData.AttackDamage, 1);
+            //m_Target.TakeDamage(TowerData.AttackDamage);
+            m_ReadyToAttack = false;
+            m_StartedCooldown = false;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        GetRMS.s_LeadCue -= Attack;
+    }
+}
