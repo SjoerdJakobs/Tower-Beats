9b66e4952236a49ad77414fd1bd0ac97907a1504
Jeffreeyy <17844@ma-web.nl>
2018-05-17 11:29:15 +0200

    V0.2 Added EffectsManager, Spawn effect for enemies now works.

--- Assets/Scripts/Managers/EffectsManager.cs
+++ Assets/Scripts/Managers/EffectsManager.cs
@@ -1,4 +1,5 @@
 ﻿using Spine.Unity;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,8 +12,9 @@ public enum EffectType
 public class EffectsManager : MonoBehaviour
 {
     public static EffectsManager s_Instance;
-    private ObjectPool m_ObjectPool;
-    [SerializeField] private SkeletonAnimation m_Prefab;
+    [SerializeField] private VisualEffect m_EffectPrefab;
+
+    private List<VisualEffect> m_EffectsPool = new List<VisualEffect>();
 
     private void Awake()
     {
@@ -21,20 +23,60 @@ public class EffectsManager : MonoBehaviour
         else
             Destroy(gameObject);
 
-        m_ObjectPool = ObjectPoolManager.s_Instance.GetObjectPool(m_Prefab.gameObject, 10, 5, 5, 10);
+        VisualEffect.s_OnEffectCompleted += OnEffectCompleted;
+
+        InitEffectsPool(10);
     }
 
-    public void SpawnEffect(EffectType type, Vector2 position)
+    private void InitEffectsPool(int amountOfEffects)
     {
-        SkeletonAnimation anim = m_ObjectPool.GetFromPool().GetComponent<SkeletonAnimation>();
+        for (int i = 0; i < amountOfEffects; i++)
+            AddEffectToPool();
+    }
 
-        switch(type)
+    private VisualEffect AddEffectToPool()
+    {
+        VisualEffect effect = Instantiate(m_EffectPrefab, transform, false);
+        effect.gameObject.SetActive(false);
+        m_EffectsPool.Add(effect);
+        return effect;
+    }
+
+    private void OnDestroy()
+    {
+        VisualEffect.s_OnEffectCompleted -= OnEffectCompleted;
+    }
+
+    public void SpawnEffect(EffectType type, bool loop, Vector2 position)
+    {
+        for (int i = 0; i < m_EffectsPool.Count; i++)
         {
-            case EffectType.ENEMY_SPAWN:
-                anim.AnimationName = "Spawn FX";
-            break;
+            if(!m_EffectsPool[i].InUse)
+            {
+                InitEffect(m_EffectsPool[i], type, loop, position);
+                return;
+            }
         }
+        VisualEffect effect = AddEffectToPool();
+        InitEffect(effect, type, loop, position);
+    }
 
-        anim.transform.position = position;
+    private void InitEffect(VisualEffect effect, EffectType type, bool loop, Vector2 position)
+    {
+        effect.gameObject.SetActive(true);
+        effect.Init(type, loop);
+        effect.transform.position = position;
+    }
+
+    private void OnEffectCompleted(VisualEffect effect)
+    {
+        for (int i = 0; i < m_EffectsPool.Count; i++)
+        {
+            if (effect == m_EffectsPool[i])
+            {
+                m_EffectsPool[i].gameObject.SetActive(false);
+                m_EffectsPool[i].InUse = false;
+            }
+        }
     }
 }
---
+++ Assets/VisualEffect.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+using Spine;
+using System;
+
+public class VisualEffect : MonoBehaviour
+{
+    private SkeletonAnimation m_Animation;
+
+    public delegate void EffectCompleted(VisualEffect effect);
+    public static EffectCompleted s_OnEffectCompleted;
+
+    public bool InUse { get; set; }
+
+    private void Awake()
+    {
+        m_Animation = GetComponent<SkeletonAnimation>();
+    }
+
+    public void Init(EffectType type, bool loop)
+    {
+        InUse = true;
+
+        switch (type)
+        {
+            case EffectType.ENEMY_SPAWN:
+                m_Animation.AnimationName = "Spawn FX";
+                break;
+        }
+
+        m_Animation.loop = loop;
+        m_Animation.AnimationState.Complete += OnEffectComplete;
+    }
+
+    private void OnEffectComplete(TrackEntry trackEntry)
+    {
+        m_Animation.AnimationState.Complete -= OnEffectComplete;
+        if (s_OnEffectCompleted != null) s_OnEffectCompleted(this);
+    }
+}
