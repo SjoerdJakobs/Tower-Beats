09e4fc816d580a40391a05507262dc5330edfc17
Lorenzo Hegberg <18927@ma-web.nl>
2018-06-08 12:31:58 +0200

    V0.3 Lead tower damage buffs, drum tower range buffs

--- Assets/Scripts/Configs/TowerConfig.cs
+++ Assets/Scripts/Configs/TowerConfig.cs
@@ -9,18 +9,18 @@ public class TowerConfig
             new TowerData(TowerTypeTags.BASS_TOWER, 1, 3, 200, 150, 75, 2f, 3.1f, 1.5f),
             new TowerData(TowerTypeTags.BASS_TOWER, 2, 3, 200, 0, 225, 2.5f, 3.1f, 1.25f),
             new TowerData(TowerTypeTags.BASS_TOWER, 3, 3, 0, 0, 375, 3.5f, 3.1f, .75f)
-        }},//1 DPS(AoE)
+        }},
         {TowerTypeTags.DRUM_TOWER, new List<TowerData>()
         {
-            new TowerData(TowerTypeTags.DRUM_TOWER, 1, 3, 200, 300, 225, 4f, 3f, 0.35f),
-            new TowerData(TowerTypeTags.DRUM_TOWER, 2, 3, 300, 0, 375, 5f,3.25f,0.3f),
-            new TowerData(TowerTypeTags.DRUM_TOWER, 3, 3, 0, 0 , 600, 7f, 3.5f, 0.25f)
-        }},//4 DPS Single target
+            new TowerData(TowerTypeTags.DRUM_TOWER, 1, 3, 200, 300, 225, 4f, 6f, 0.35f),
+            new TowerData(TowerTypeTags.DRUM_TOWER, 2, 3, 300, 0, 375, 5f, 8f,0.3f),
+            new TowerData(TowerTypeTags.DRUM_TOWER, 3, 3, 0, 0 , 600, 7f, 10f, 0.25f)
+        }},
         {TowerTypeTags.LEAD_TOWER, new List<TowerData>()
-        {  //10 DPS Single target
-            new TowerData(TowerTypeTags.LEAD_TOWER, 1, 3, 200, 200, 150, 0.25f, 3.25f, 0.025f),
-            new TowerData(TowerTypeTags.LEAD_TOWER, 2, 3, 300, 0, 300, 0.5f, 3.5f, 0.025f),
-            new TowerData(TowerTypeTags.LEAD_TOWER, 3, 3, 0, 0, 525, 0.75f, 3.75f, 0.025f)
+        {  
+            new TowerData(TowerTypeTags.LEAD_TOWER, 1, 3, 200, 200, 150, 0.75f, 3.25f, 0.025f),
+            new TowerData(TowerTypeTags.LEAD_TOWER, 2, 3, 300, 0, 300, 1.25f, 3.5f, 0.025f),
+            new TowerData(TowerTypeTags.LEAD_TOWER, 3, 3, 0, 0, 525, 2f, 3.75f, 0.025f)
         }}
     };
 }//
