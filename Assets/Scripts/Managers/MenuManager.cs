e3a1b785d7d909d618e65420c1e28f3c660415b4
Lorenzo Hegberg <18927@ma-web.nl>
2018-06-08 17:01:29 +0200

    v0.3 Fixed reload scene retry

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -134,6 +134,11 @@ public class MenuManager : MonoBehaviour
 
     public void LoadScene(string sceneName)
     {
-        Sceneloader.s_Instance.LoadGameSceneWithLevel(Sceneloader.s_Instance.m_LevelToLoad);
+        Sceneloader.s_Instance.LoadScene(sceneName);
+    }
+
+    public void ReloadScene()
+    {
+        Sceneloader.s_Instance.LoadGameSceneWithLevel(GameManager.s_Instance.LoadedLevel);
     }
 }
