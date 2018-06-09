7b13f9b31c6b40acee43b3e361dc120605fa23bf
Jeffreeyy <17844@ma-web.nl>
2018-06-09 17:39:20 +0200

    V0.3 Finishing touches

---
+++ Assets/PauseButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseButton : MonoBehaviour
+{
+    public bool Paused { get; set; }
+
+    [SerializeField]private GameObject m_PauseIcon;
+    [SerializeField]private GameObject m_PlayIcon;
+
+    private void Start()
+    {
+        HandleIcon();
+    }
+
+    public void TogglePause()
+    {
+        Paused = !Paused;
+        PauseCheck.s_Instance.TogglePause();
+        HandleIcon();
+    }
+
+    private void HandleIcon()
+    {
+        m_PauseIcon.SetActive(!Paused);
+        m_PlayIcon.SetActive(Paused);
+    }
+}
