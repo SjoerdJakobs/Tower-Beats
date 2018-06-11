7a8fc1ab1aaf7815b14bba90baf794ebcefd0498
Jeffreeyy <17844@ma-web.nl>
2018-06-11 16:41:39 +0200

    V0.3 Code commenting

---
+++ Assets/Scripts/Audio/Song.cs
@@ -0,0 +1,20 @@
+﻿/// <summary>
+/// Songs class.
+/// </summary>
+[System.Serializable]
+public class Song
+{
+    /// <summary>
+    /// Constructor of the Song
+    /// </summary>
+    /// <param name="songName">Name of the Song</param>
+    public Song(string songName)
+    {
+        Songname = songName;
+    }
+
+    /// <summary>
+    /// Song name
+    /// </summary>
+    public string Songname;
+}
--- Assets/Scripts/UI/MainMenu/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu/MainMenu.cs
@@ -1,77 +1,159 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    #region Variables
+
     [SerializeField] private Transform[] m_Buttons;
     [SerializeField] private Credits m_Credits;
     [SerializeField] private RectTransform m_PlatformContainer;
     [SerializeField] private CanvasGroup m_PlatformAlpha;
 
-    public void LoadScene(string scene)
-    {
-        Sceneloader.s_Instance.LoadScene(scene);
-        SoundManager.s_Instance.PlaySound(SoundNames.BUTTON_CLICK);
-    }
+    #endregion
 
-    public void ShowCredits()
-    {
-        StartCoroutine(AnimateButtons(false));
-        m_Credits.gameObject.SetActive(true);
-        SoundManager.s_Instance.PlaySound(SoundNames.BUTTON_CLICK);
-    }
+    #region Monobehaviour Functions
 
-    public void HideCredits()
-    {
-        ShowMainButtons();
-        m_Credits.gameObject.SetActive(false);
-        SoundManager.s_Instance.PlaySound(SoundNames.BUTTON_CLICK);
-    }
-
+    /// <summary>
+    /// Gets called when the GameObject initializes
+    /// </summary>
     private void Start()
     {
-        if(!SoundManager.s_Instance.IsSoundPlaying(SoundNames.BACKGROUND_MUSIC))
+        // Play the background music if it isn't playing already
+        if (!SoundManager.s_Instance.IsSoundPlaying(SoundNames.BACKGROUND_MUSIC))
             SoundManager.s_Instance.PlaySound(SoundNames.BACKGROUND_MUSIC, true);
 
+        // Show the Main Menu buttons
         ShowMainButtons();
+
+        // Show the Don Diablo Avatar
         ShowDonDiablo();
     }
 
+    #endregion
 
+    #region Sceneloading
+
+    /// <summary>
+    /// Load a Scene (Calls the Sceneloader Instance)
+    /// </summary>
+    /// <param name="scene">Scene name</param>
+    public void LoadScene(string scene)
+    {
+        // Load the scene
+        Sceneloader.s_Instance.LoadScene(scene);
+
+        // Play button click sound
+        SoundManager.s_Instance.PlaySound(SoundNames.BUTTON_CLICK);
+    }
+
+    #endregion
+
+    #region Credits
+
+    /// <summary>
+    /// Show the Credits
+    /// </summary>
+    public void ShowCredits()
+    {
+        // Hide the buttons
+        StartCoroutine(AnimateButtons(false));
+
+        // Set the credits active
+        m_Credits.gameObject.SetActive(true);
+
+        // Play button click sound
+        SoundManager.s_Instance.PlaySound(SoundNames.BUTTON_CLICK);
+    }
+
+    /// <summary>
+    /// Hide the Credits
+    /// </summary>
+    public void HideCredits()
+    {
+        // Show the Main Menu buttons
+        ShowMainButtons();
+
+        // Hide the Credits
+        m_Credits.gameObject.SetActive(false);
+
+        // Play button click sound
+        SoundManager.s_Instance.PlaySound(SoundNames.BUTTON_CLICK);
+    }
+
+    #endregion
+
+    #region Quitting
+
+    /// <summary>
+    /// Quit the Game
+    /// </summary>
     public void QuitGame()
     {
+        // Play button click sound
         SoundManager.s_Instance.PlaySound(SoundNames.BUTTON_CLICK);
+
+        // Quit the Application
         Application.Quit();
     }
 
+    #endregion
+
+    #region Don Diablo Avatar
+
+    /// <summary>
+    /// Sets the default state of Don Diablo's avatar
+    /// </summary>
     private void SetDonDiabloDefaultState()
     {
+        // Reset to default position
         m_PlatformContainer.anchoredPosition = new Vector2(-25f, -400f);
+
+        // Hide with alpha on 0
         m_PlatformAlpha.alpha = 0;
     }
 
+    /// <summary>
+    /// Shows Don Diablo's Avatar
+    /// </summary>
     private void ShowDonDiablo()
     {
+        // Set the default state
         SetDonDiabloDefaultState();
+
+        // Animate Don Diablo's avatar
         m_PlatformContainer.DOAnchorPosY(0, 0.5f).SetEase(Ease.OutExpo);
         m_PlatformAlpha.DOFade(1, 0.33f);
     }
 
+    #endregion
+
+    #region Main Menu Buttons
+
+    /// <summary>
+    /// Show the Main Menu Buttons
+    /// </summary>
     public void ShowMainButtons()
     {
         SetButtonsDefaultState();
         StartCoroutine(AnimateButtons(true));
     }
 
+    /// <summary>
+    /// Set the default state of the Main Menu Buttons
+    /// </summary>
     private void SetButtonsDefaultState()
     {
         for (int i = 0; i < m_Buttons.Length; i++)
             m_Buttons[i].localScale = Vector2.zero;
     }
 
+    /// <summary>
+    /// Animate the Main Menu Buttons
+    /// </summary>
+    /// <param name="animateIn">True == Animate in, false == animate out</param>
+    /// <returns></returns>
     private IEnumerator AnimateButtons(bool animateIn)
     {
         for (int i = 0; i < m_Buttons.Length; i++)
@@ -80,4 +162,6 @@ public class MainMenu : MonoBehaviour
             yield return new WaitForSeconds(0.05f);
         }
     }
+
+    #endregion
 }
